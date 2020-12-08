2a6403ba6f69ee9d7adaa58510fe09e2d1a35c32
leonway <17318077256@163.com>
2020-12-08 20:53:39 +0800

    修改第三节课答案

--- 03-net-coding/homework/index.js
+++ 03-net-coding/homework/index.js
@@ -1,22 +1,53 @@
 const path = require('path')
 const fs = require('fs')
 
+//原始答案
+// const parser = async (originPath)=>{
+//   return new Promise((res,rej)=>{
+//     let data = Buffer.from('')
+//     const filePath = path.resolve(originPath)
+//     try {
+//       if(fs.existsSync(filePath)){
+//         const stream = fs.createReadStream(filePath)
+//         stream.on('data',chunk=>{
+//           // console.log('data',data)
+//           // console.log('chun');
+          
+//           data += chunk
+//         })
+//         stream.on('close',()=>{
+//           // console.log('---data-------');
+//           // console.log(data);
+//           // console.log('----data.toString()-----');
+//           // console.log(data.toString());
+//           res(JSON.parse(data))          
+//         })
+//       }
+//     } catch (e) {
+//       console.log('parser error');
+//       console.log(e);
+//       rej(e)
+//     }
+//   })
+// }
+
 const parser = async (originPath)=>{
+  let reqData = []
+  let size = 0
   return new Promise((res,rej)=>{
-    let data = Buffer.from('')
     const filePath = path.resolve(originPath)
     try {
       if(fs.existsSync(filePath)){
         const stream = fs.createReadStream(filePath)
         stream.on('data',chunk=>{
-          data += chunk
+          // console.log('chunk',chunk);
+          
+          reqData.push(chunk)
+          size+=chunk.length
         })
         stream.on('close',()=>{
-          // console.log('---data-------');
-          // console.log(data);
-          // console.log('----data.toString()-----');
-          // console.log(data.toString());
-          res(JSON.parse(data))          
+         const data = Buffer.concat(reqData,size)
+          res(JSON.parse(data.toString()))          
         })
       }
     } catch (e) {
@@ -26,7 +57,6 @@ const parser = async (originPath)=>{
     }
   })
 }
-
 module.exports = {
   parser
 }
