59894a684eee65a112584a61a0ef7c39c931a303
reamey <17318077256@163.com>
2020-12-11 11:04:08 +0800

    完成第六节课内容以及作业

--- 06-authentication/self-actulization/index.js
+++ 06-authentication/self-actulization/index.js
@@ -2,17 +2,28 @@ const Koa = require('koa')
 const app = new Koa()
 const session = require('koa-session')
 
+const redisStore = require('koa-redis')
+const redis = require('redis')
+const redisClient = redis.createClient(6379,'localhost')
+
+const wrapper = require('co-redis')
+const client = wrapper(redisClient)
+
 app.keys = ['xcsfdsaf']
 
 const SESS_CONFIG = {
   key:'kkb:sess',
-  maxAge:8640000,
+  store:redisStore({client}),
   httpOnly:true,
   signed:false
 }
 
 app.use(session(SESS_CONFIG, app))
-
+app.use(async(ctx,next)=>{
+  const keys = await client.keys('*')
+  keys.forEach(async(key)=>console.log(key,await client.get(key) ))
+  await next()
+})
 // 测试
 
 app.use(ctx=>{
